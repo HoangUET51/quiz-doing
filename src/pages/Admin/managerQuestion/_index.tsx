d726c5e49426371f7e0e70d23e1344d8014d8c10
hoang.vuvan <vuvanhoanguet51@gmail.com>
2022-11-21 23:15:44 +0700

    assgin-quiz-for-user

--- src/pages/Admin/managerQuestion/_index.tsx
+++ src/pages/Admin/managerQuestion/_index.tsx
@@ -4,7 +4,6 @@ import AddQuiz from "../../../components/blocks/AddQuiz/AddQuiz";
 export default function ManagerQuestions() {
   return (
     <div className="manager-container mx-3">
-      <div className="title my-2 text-xl font-semibold">Add Manager Quiz</div>
       <div className="content">
         <AddQuiz />
       </div>
